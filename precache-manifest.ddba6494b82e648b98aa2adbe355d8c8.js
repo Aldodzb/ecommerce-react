dec46e5d734af4588dfef6970452bc0f03078cfd
Aldo <aldodz@gmail.com>
2019-07-31 00:25:44 -0300

    Updates

---
+++ precache-manifest.ddba6494b82e648b98aa2adbe355d8c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f53f350f1f49ecbef852aeeaabdae47",
+    "url": "/ecommerce-react/index.html"
+  },
+  {
+    "revision": "491644ba4a552590df88",
+    "url": "/ecommerce-react/static/css/2.56d9453f.chunk.css"
+  },
+  {
+    "revision": "6f487683e7bdd27be587",
+    "url": "/ecommerce-react/static/css/main.bcd85937.chunk.css"
+  },
+  {
+    "revision": "491644ba4a552590df88",
+    "url": "/ecommerce-react/static/js/2.349482aa.chunk.js"
+  },
+  {
+    "revision": "6f487683e7bdd27be587",
+    "url": "/ecommerce-react/static/js/main.2c37fdbc.chunk.js"
+  },
+  {
+    "revision": "e7e101a2f13923c20bcd",
+    "url": "/ecommerce-react/static/js/runtime~main.af09581d.js"
+  }
+]);
